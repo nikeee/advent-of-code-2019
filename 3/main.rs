749e015bf4849929fbcffa0d524c7a1da21ddbc8
Niklas Mollenhauer <nikeee@outlook.com>
2019-12-03 21:33:31 +0100

    Clean up solution

--- 3/main.rs
+++ 3/main.rs
@@ -1,19 +1,22 @@
 // Compile:
-// rustc -o 3.out main.rs
+// rustc -O -o 3.out main.rs
 // Run:
 // ./3.out < input.txt
 
-use std::io;
-use std::vec;
-use std::collections::HashSet;
-use std::collections::HashMap;
+use std::{io, collections::HashMap, error::Error};
+
+#[derive(Debug)]
+struct Path {
+	amount: i32,
+	direction: Direction,
+}
 
 #[derive(Debug)]
 enum Direction {
-	Up { amount: i32 },
-	Down { amount: i32 },
-	Left { amount: i32 },
-	Right { amount: i32 },
+	Up,
+	Down,
+	Left,
+	Right,
 }
 
 #[derive(Hash, Eq, PartialEq, Clone, Copy, Debug)]
@@ -28,19 +31,20 @@ impl Point {
 }
 
 
-fn main() -> io::Result<()> {
+fn main() -> Result<(), Box<dyn Error>> {
 
-	let line_0 = read_parsed_directions();
-	let line_1 = read_parsed_directions();
+	let line_0 = read_parsed_paths()?;
+	let line_1 = read_parsed_paths()?;
 
 	let origin = Point {x: 0, y: 0};
 
-	let (line_0_points, line_0_steps) = get_visited_points(line_0, origin);
-	let (line_1_points, line_1_steps) = get_visited_points(line_1, origin);
+	let line_0_step_map = get_visited_points(line_0, origin);
+	let line_1_step_map = get_visited_points(line_1, origin);
 
 	// Part 1
-	let min_distance_to_origin = line_0_points.intersection(&line_1_points)
-			.map(|common_point| origin.manhatten_distance_to(&common_point))
+	let min_distance_to_origin = line_0_step_map.iter()
+			.filter(|(point, _)| line_1_step_map.contains_key(point))
+			.map(|(common_point, _)| origin.manhatten_distance_to(&common_point))
 			.min();
 
 	match min_distance_to_origin {
@@ -49,115 +53,94 @@ fn main() -> io::Result<()> {
 	}
 
 	// Part 2
-	let min_distance_in_steps = line_0_points.intersection(&line_1_points)
-			.map(|common_point| match (line_0_steps.get(common_point), line_1_steps.get(common_point)) {
-				(Some(s0), Some(s1)) => Some(s0 + s1),
-				_ => None
-			})
+	let min_distance_in_steps = line_0_step_map.iter()
+			.filter_map(|(point, line_0_steps)| line_1_step_map.get(point).map(|line_1_steps| (line_0_steps, line_1_steps)))
+			.map(|(c0, c1)| c0 + c1)
 			.min();
 
 	match min_distance_in_steps {
-		Some(Some(distance)) => println!("Part 2: The fewest combined steps the wires must take to reach an intersection: {}", distance),
+		Some(distance) => println!("Part 2: The fewest combined steps the wires must take to reach an intersection: {}", distance),
 		_ => println!("No intersection found"),
 	}
 
 	Ok(())
 }
 
-fn read_parsed_directions() -> vec::Vec<Direction> {
+fn read_parsed_paths() -> Result<Vec<Path>, Box<dyn Error>> {
 	let mut line = String::new();
-	io::stdin().read_line(&mut line).unwrap();
-	return parse_cable(line.trim().to_string());
+	io::stdin().read_line(&mut line)?;
+
+	parse_cable(line.trim().to_string())
 }
 
-fn get_visited_points(directions: vec::Vec<Direction>, origin: Point) -> (HashSet<Point>, HashMap<Point, u32>) {
-	let mut res = HashSet::new();
+fn get_visited_points(paths: Vec<Path>, origin: Point) -> HashMap<Point, u32> {
+	use Direction::*;
+
 	let mut steps_map = HashMap::new();
 
 	let mut location = origin;
 	let mut current_step_count: u32 = 0;
 
-	for op in directions {
-		match op {
-			Direction::Up{amount} => {
-				for _ in 0..amount {
-					current_step_count += 1;
-					location.y += 1;
-					let here = Point {x: location.x, y: location.y};
+	for path in paths {
 
-					res.insert(here);
-					if !steps_map.contains_key(&here) {
-						steps_map.insert(here, current_step_count);
-					}
-				}
-			},
-			Direction::Down{amount} => {
-				for _ in 0..amount {
-					current_step_count += 1;
-					location.y -= 1;
-					let here = Point {x: location.x, y: location.y};
-
-					res.insert(here);
-					if !steps_map.contains_key(&here) {
-						steps_map.insert(here, current_step_count);
-					}
-				}
-			},
-			Direction::Left{amount} => {
-				for _ in 0..amount {
-					current_step_count += 1;
-					location.x -= 1;
-					let here = Point {x: location.x, y: location.y};
-
-					res.insert(here);
-					if !steps_map.contains_key(&here) {
-						steps_map.insert(here, current_step_count);
-					}
-				}
-			},
-			Direction::Right{amount} => {
-				for _ in 0..amount {
-					current_step_count += 1;
-					location.x += 1;
-					let here = Point {x: location.x, y: location.y};
-
-					res.insert(here);
-					if !steps_map.contains_key(&here) {
-						steps_map.insert(here, current_step_count);
-					}
-				}
-			},
+		let move_point: fn (&mut Point) = match path.direction {
+			Up => |p| p.y += 1,
+			Down => |p| p.y -= 1,
+			Left => |p| p.x -= 1,
+			Right => |p| p.x += 1,
 		};
+
+		for _ in 0..path.amount {
+			current_step_count += 1;
+			move_point(&mut location);
+			steps_map.entry(location).or_insert(current_step_count);
+		}
 	}
 
-	return (res, steps_map);
+	steps_map
 }
 
-fn parse_cable(line: String) -> vec::Vec<Direction> {
-	return line.split(",")
-				.map(String::from)
-				.map(|command| {
+fn parse_cable(line: String) -> Result<Vec<Path>, Box<dyn Error>> {
+	use Direction::*;
 
-					let dir = command.chars().next().unwrap();
+	line.split(",")
+				.map(String::from)
+				.filter(|s| !s.is_empty())
+				.map(|command| {
+					let dir = command.chars().next().unwrap().to_ascii_uppercase();
 
 					let amount_str = without_first(&command);
-					let amount = amount_str.parse::<i32>().unwrap();
+					let amount = amount_str.parse::<i32>()?;
 
-					return match dir  {
-						'R' | 'r' => Direction::Right{amount},
-						'U' | 'u' => Direction::Up{amount},
-						'D' | 'd' => Direction::Down{amount},
-						'L' | 'l' => Direction::Left{amount},
-						_ => panic!(),
-					}
+					let res = Path {
+						amount,
+						direction: match dir {
+							'R' => Right,
+							'U' => Up,
+							'D' => Down,
+							'L' => Left,
+							_ => Err(InvalidDirection(dir))?,
+						},
+					};
+
+					Ok(res)
 				})
-				.collect();
+				.collect()
 }
 
+#[derive(Debug)]
+struct InvalidDirection(char);
+impl std::fmt::Display for InvalidDirection {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		write!(f, "Unable to parse direction '{}'", self.0)
+	}
+}
+impl Error for InvalidDirection {}
+
 fn without_first(string: &str) -> &str {
-    string
-        .char_indices()
-        .nth(1)
-        .and_then(|(i, _)| string.get(i..))
-        .unwrap_or("")
+	string
+		.char_indices()
+		.nth(1)
+		.and_then(|(i, _)| string.get(i..))
+		.unwrap_or("")
 }
