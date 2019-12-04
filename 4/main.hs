c9a120cc3b1b2943c71d5fa7fa5f44d8746fe47c
Niklas Mollenhauer <nikeee@outlook.com>
2019-12-04 21:14:52 +0100

    Solve 4: 2/2

--- 4/main.hs
+++ 4/main.hs
@@ -1,7 +1,9 @@
 -- Compile:
--- ghc -o 4.out main.hs
+-- ghc -O3 -o 4.out main.hs
 -- Run:
 -- ./4.out
+
+import Data.List
 
 -- "Your puzzle input is 307237-769058."
 input_range = [307237..769058]
@@ -23,15 +25,40 @@ is_ascending [] = True
 is_ascending [_] = True
 is_ascending (a:b:xs) = a <= b && is_ascending (b:xs)
 
+digits = ['0'..'9']
+is_possible_digit :: String -> Char -> Bool
+is_possible_digit candidate digit = (isInfixOf [digit, digit] candidate) && not (isInfixOf [digit, digit, digit] candidate)
+
+-- "the two adjacent matching digits are not part of a larger group of matching digits."
+-- We use a trick here:
+--     We check if "123446" contains "44" and does not contain "444" (for every digit)
+--     It is possible because a valid repetition consists out of two digits. If there is a third, the occurrence is not valid. There has to be at leat one, though.
+--     The cases of "112111" or "111211" won't happen because the candidates are also filtered to be ascending.
+--     Also, "111111" is invalid.
+--     "111233" is valid.
+has_equal_adjacent_chars_not_in_cluster :: String -> Bool
+has_equal_adjacent_chars_not_in_cluster candidate = any (candidate `is_possible_digit`) digits
+
 -- The order in filtering is not relevant.
 -- So we put the filter that eliminates the most candidates first.
-part_1 = filter is_ascending (
+part_1 input = filter is_ascending (
 			filter has_equal_adjacent_chars (
 				filter length_6 (
-					map show input_range
+					map show input
 				)
 			)
 		)
 
-main =
-	putStrLn ("Part 1: " ++ (show (length part_1)))
+
+part_2 input = filter is_ascending (
+			filter has_equal_adjacent_chars_not_in_cluster (
+				filter length_6 (
+					map show input
+				)
+			)
+		)
+
+main :: IO ()
+main = do
+	putStrLn ("Part 1: " ++ (show (length (part_1 input_range))))
+	putStrLn ("Part 2: " ++ (show (length (part_2 input_range))))
