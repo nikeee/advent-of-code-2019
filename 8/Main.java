3644717c31b4f29fae79e704c85a3f88f46e66ef
Niklas Mollenhauer <nikeee@outlook.com>
2019-12-08 14:37:21 +0100

    Solve 8: 2/2

--- 8/Main.java
+++ 8/Main.java
@@ -27,7 +27,56 @@ public class Main {
 			} else {
 				System.out.println("Part 1 solution: " + part1Solution.get());
 			}
+
+			var image = mergeLayers(layers);
+
+			System.out.println("Part 2 solution:");
+			printImage(image);
 		}
+	}
+
+	static void printImage(int[][] image) {
+		for (int[] column : image) {
+			for (int pixel : column) {
+				var pre = pixel == 1 ? "\u001B[40m" : "\u001B[107m";
+				System.out.print(pre + " " + "\u001B[0m");
+			}
+			System.out.println();
+		}
+	}
+
+	static int[][] mergeLayers(List<int[][]> layers) {
+		var res = new int[IMAGE_HEIGHT][IMAGE_WIDTH];
+
+		// Prime the image as transparent
+		for (var layer : layers) {
+			for (int y = 0; y < layer.length; ++y) {
+				for (int x = 0; x < layer[y].length; ++x) {
+					res[y][x] = 2;
+				}
+			}
+		}
+
+		for (var layer : layers) {
+			for (int y = 0; y < layer.length; ++y) {
+				for (int x = 0; x < layer[y].length; ++x) {
+					res[y][x] = mergeColors(res[y][x], layer[y][x]);
+				}
+			}
+		}
+
+		return res;
+	}
+
+	static int mergeColors(int existingColor, int colorBelow) {
+		if (existingColor != 2) // Color is already set, just take this one
+			return existingColor;
+
+		if (colorBelow == 1)
+			return 1;
+		if (colorBelow == 0)
+			return 0;
+		return existingColor;
 	}
 
 	static List<int[][]> readLayers(String input) {
